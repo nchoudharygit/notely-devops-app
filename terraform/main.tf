a0aa5efe775dd343a55ff4e9924d2cd9c8d50f72
Neha choudhary <nehachoudhary1108@gmail.com>
2026-04-13 12:04:47 +0530

    added terraform validate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "sg" {
+resourccccce "aws_security_group" "sg" {
   name = "test"
 
   ingress { from_port = 80 to_port = 80 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
