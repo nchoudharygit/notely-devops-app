3d1db281ee9858c07cc77f13014ff9117eaff1f6
Neha choudhary <nehachoudhary1108@gmail.com>
2026-04-13 11:48:00 +0530

    added force fail

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "example" { ami="ami-123456" instance_type="t2.micro"
+resource "aws_security_group" "sg" {
+  name = "test"
 
-tags={Name="Test"}
+  ingress { from_port = 80 to_port = 80 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
 }
