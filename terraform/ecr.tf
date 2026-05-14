ec1adca9f314112748ab09124c20cfd46c616e85
Neha choudhary <nehachoudhary1108@gmail.com>
2026-05-14 17:29:24 +0530

    removed extra ecr creation code from deploy yml

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -12,6 +12,13 @@ resource "aws_ecr_lifecycle_policy" "notely" {
   action = { type = "expire" } }] })
 }
 output "ecr_url" { value = aws_ecr_repository.notely.repository_url }
-
+resource "aws_ecr_repository" "nginx" {
+  name                 = "notely-nginx"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true # Caution: deletes all images when repo is deleted
+  image_scanning_configuration { scan_on_push = true }
+  tags = { Name = "notely-nginx", Environment = var.environment }
+}
+output "nginx_ecr_url" { value = aws_ecr_repository.nginx.repository_url }
 # To authenticate Docker to ECR, use the following command:
 # aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws_account_id>.dkr.ecr.<region>.amazonaws.com
