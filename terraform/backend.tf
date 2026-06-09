d9c3628e350e5910e3bcc525d530a40bc850557b
Neha choudhary <nehachoudhary1108@gmail.com>
2026-06-09 17:09:07 +0530

    fix: backend.tf + variables default + ecr state import fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "s3" {
-  
+    bucket       = "notely-terraform-state-162185499985"
+    key          = "staging/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
